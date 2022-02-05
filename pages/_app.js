99294db7fd5cbfdf645740cd7ae76d65c596069f
yasi <zahrafarrokhi2017@gmail.com>
2022-02-05 18:45:09 +0330

    Setup nextjs layout config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page)
+  return getLayout(<Component {...pageProps} />)
 }
-
 export default MyApp
