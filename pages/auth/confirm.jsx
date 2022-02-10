45dbc126272fd0f2aa402352b0331bb035fc0a50
yasi <zahrafarrokhi2017@gmail.com>
2022-02-10 13:37:45 +0330

    Add timer

--- pages/auth/confirm.jsx
+++ pages/auth/confirm.jsx
@@ -2,21 +2,26 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../../components/Layout';
 import VerificationInput from 'react-verification-input';
-import { authSlice, confirm } from '../../lib/slices/auth';
+import { authSlice, confirm, requestMobileOTP } from '../../lib/slices/auth';
 import styles from '../../styles/Confirm.module.scss'
 import { persianToEnglishDigits } from '../../lib/utils';
 import { useRouter } from 'next/dist/client/router';
 import { GrRefresh } from 'react-icons/gr';
 
+const CODE_LENGTH = 4;
+const EXP_TIME = 120;
 
 const Confirm = (props) => {
-  const CODE_LENGTH = 4;
+  
+
   const [code, setCode] = useState('');
   const [error, setError] = useState(false);
   const username = useSelector((state)=>state?.authReducer?.username);
   const method = useSelector((state) => state?.authReducer?.method);
   const dispatch = useDispatch();
   const router = useRouter();
+  const [time, setTime] = useState(EXP_TIME);
+  const timerRef = useRef(null);
 
   const submit = async () => {
     try {
@@ -25,6 +30,40 @@ const Confirm = (props) => {
         confirm({ method, token: code, [method]: username }),
       ).unwrap();
       router.push('/patients/');
+    } catch (e) {
+      setError(true);
+    }
+  };
+
+  const startTimer = () => {
+    if (timerRef.current) clearInterval(timerRef.current);
+    const timerInterval = setInterval(() => {
+      if (time > 0) setTime((t) => (t > 0 ? t - 1 : t));
+      else clearInterval(timerRef.current);
+    }, 1000);
+    timerRef.current = timerInterval;
+  };
+
+  useEffect(() => {
+    if (!method || !username) {
+      router.push('/auth/login');
+      return;
+    }
+//open page =>rendering page on mount
+    startTimer();
+// close page =>on unmount
+    return () => {
+      if (timerRef.current) clearInterval(timerRef.current);
+    };
+  }, []);
+
+  const resendCode = async () => {
+    if (time > 0) return;
+    try {
+      await dispatch(requestMobileOTP(username)).unwrap();
+      console.log("Resending", time)
+      setTime(EXP_TIME);
+      // startTimer();
     } catch (e) {
       setError(true);
     }
@@ -64,7 +103,7 @@ const Confirm = (props) => {
             <button
               className={`btn btn-primary ${styles.btn}`}
               onClick={submit}
-              disabled={code.length !== 4}
+              disabled={time ===0 || code.length !== 4}
             >
               ادامه
   
@@ -72,16 +111,19 @@ const Confirm = (props) => {
           </div>
           <div className={`p-2 d-flex   d-column justify-content-between ${styles.refreshcnt}`}>
           <div
-            className={`d-flex  ${styles.resendcode}`}
-        
-          
+             className={`d-flex  ${styles.resendcode}`}
+             onClick={resendCode}
+             disabled={time !== 0}
           >
             ارسال مجدد
           </div>
           <div className="d-flex ">
             <GrRefresh className={`${styles.refresh}`} />
-           
-
+           {/* padStart -> Add padding to string : 4->04, 0->00, 12->12 */}
+           {/* min:second */}
+            {String(Math.floor(time / 60)).padStart(2, '0')}
+            :
+            {String(time % 60).padStart(2, '0')}
           </div>
         </div>
     
