76ea3e91ae45b2e26530c7a6ade81cbfbb6cf80e
yasi <zahrafarrokhi2017@gmail.com>
2022-02-10 12:59:56 +0330

    fix style layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = (props)=>{
   const {children,backLink} = props; // const children = props.children
   return(
     <div className={`d-flex flex-row justify-content-center align-items-center ${styles.cnt} `}>
-      <div className={`d-flex flex-column justify-content-center align-items-center ${styles.cnt_init}`}>
+      <div className={`d-flex flex-column justify-content-center align-items-center align-self-stretch ${styles.cnt_init}`}>
       <IoChevronBackCircleOutline className={backLink ? styles.back_link : 'd-none'}/>
       {children}
       </div>
